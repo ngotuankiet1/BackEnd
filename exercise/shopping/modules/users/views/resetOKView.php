d8d63c78f7b224f4b25ecb6a657fd96d2ff4d860
Asus <Asus@DESKTOP-7VDO8JQ>
2022-04-27 19:48:07 +0700

    backend

---
+++ exercise/shopping/modules/users/views/resetOKView.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Hệ thống điều hướng cơ bản</title>
+        <link href="public/css/reset.css" rel="stylesheet" type="text/css"/>
+        <link href="public/css/style-reg.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div id="wrapper-form-reg">
+            <h1 class="page-title">KHÔI PHỤC MẬT KHẨU</h1>
+            <p>Bạn đã thay đổi mật khẩu thành công vui lòng <a style="font-weight: bold" href="?mod=users&action=login">Click</a> để quay về trang đăng nhập</p>
+        </div>
+    </body>
+</html>
